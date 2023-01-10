10a6084b36a7bc9c7aefe78b094b414fcf75968f
Seth Levine <sethlevi@amazon.com>
2023-01-10 12:01:54 -0500

    r

--- auto_update/auto_update.go
+++ auto_update/auto_update.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/aws/amazon-cloudwatch-agent-test/internal/common"
+	"fmt"
 	"log"
 	"os/exec"
+	"path/filepath"
 	"time"
 )
 
@@ -16,8 +17,8 @@ func main() {
 	for true {
 		syncAgent()
 		installAgent()
-		common.CopyFile(configInput, configOutputPath)
-		err := common.StartAgent(configOutputPath, true)
+		CopyFile(configInput, configOutputPath)
+		err := StartAgent(configOutputPath, true)
 		if err != nil {
 			log.Printf("Err starting agent %v", err)
 		}
@@ -38,4 +39,38 @@ func installAgent() {
 	if err != nil {
 		log.Printf("Failed to install agent err %v", err)
 	}
+}
+
+func CopyFile(pathIn string, pathOut string) {
+	log.Printf("Copy File %s to %s", pathIn, pathOut)
+	pathInAbs, err := filepath.Abs(pathIn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("File %s abs path %s", pathIn, pathInAbs)
+	out, err := exec.Command("bash", "-c", "sudo cp "+pathInAbs+" "+pathOut).Output()
+
+	if err != nil {
+		log.Fatal(fmt.Sprint(err) + string(out))
+	}
+
+	log.Printf("File : %s copied to : %s", pathIn, pathOut)
+}
+
+func StartAgent(configOutputPath string, fatalOnFailure bool) error {
+	out, err := exec.
+		Command("bash", "-c", "sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:"+configOutputPath).
+		Output()
+
+	if err != nil && fatalOnFailure {
+		log.Fatal(fmt.Sprint(err) + string(out))
+	} else if err != nil {
+		log.Printf(fmt.Sprint(err) + string(out))
+	} else {
+		log.Printf("Agent has started")
+	}
+
+	return err
 }
